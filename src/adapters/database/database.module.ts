65f1fdb1b2deda67e75bcc38c9de352922256078
Yurii <yuraovdnk1999@ukr.net>
2023-04-28 18:38:52 +0300

    fix: db url

--- src/adapters/database/database.module.ts
+++ src/adapters/database/database.module.ts
@@ -6,19 +6,22 @@ import { BanUserSubscriber } from '../../modules/users/domain/subscribers/banUse
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        subscribers: [BanUserSubscriber],
-        extra: {
-          poolSize: 4,
-        },
-        url:
-          process.env.NODE_ENV === 'production'
-            ? configService.get<string>('db.postgresUriProduction')
-            : configService.get<string>('db.postgresUriDev'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        console.log(process.env.NODE_ENV, 'ENVIROMNET');
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          subscribers: [BanUserSubscriber],
+          extra: {
+            poolSize: 4,
+          },
+          url:
+            process.env.NODE_ENV === 'production'
+              ? configService.get<string>('db.postgresUriProduction')
+              : configService.get<string>('db.postgresUriDev'),
+        };
+      },
 
       inject: [ConfigService],
     }),
