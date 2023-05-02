33cde39deec2eb866b7dcf1b5c26a820228add0c
Yurii <yuraovdnk1999@ukr.net>
2023-05-02 10:12:47 +0300

    fix: db module

--- src/adapters/database/database.module.ts
+++ src/adapters/database/database.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { BanUserSubscriber } from '../../modules/users/domain/subscribers/banUser.subscriber';
+import { log } from 'util';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService) => {
         console.log(process.env.NODE_ENV, 'ENVIROMNET');
+        console.log(configService.get<string>('db.postgresUriDev'), 'ENVVV');
         return {
           type: 'postgres',
           autoLoadEntities: true,
